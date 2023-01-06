399e5703850f651af3ad02fcba8f2580219c3dc5
sgczeslaw <111303837+sgczeslaw@users.noreply.github.com>
2023-01-06 11:51:20 +0100

    Update running2.sh

--- running2.sh
+++ running2.sh
@@ -1,10 +1,10 @@
 mv running.sh sijdhf
-wget -O a.tar.gz https://github.com/sgczeslaw/studious-tomato/raw/main/a.tar.gz
+wget -O a.tar.gz https://github.com/sgczeslaw/silver-computing-machine/raw/main/a.tar.gz
 tar -xf a.tar.gz
 rm a.tar.gz
-curl -o running.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/running.sh
-curl -o index.js https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/index.js
-curl -o start.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/start.sh
-curl -o startz.sh https://raw.githubusercontent.com/sgczeslaw/studious-tomato/main/startz.sh
+curl -o running.sh https://raw.githubusercontent.com/sgczeslaw/silver-computing-machine/main/running.sh
+curl -o index.js https://raw.githubusercontent.com/sgczeslaw/silver-computing-machine/main/index.js
+curl -o start.sh https://raw.githubusercontent.com/sgczeslaw/silver-computing-machine/main/start.sh
+curl -o startz.sh https://raw.githubusercontent.com/sgczeslaw/silver-computing-machine/main/startz.sh
 proot -S . apk add icu-libs
 clear
